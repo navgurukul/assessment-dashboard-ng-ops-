6658e30a262b0a575feb75263c8e265914e71044
Prabhat Lodhi <prabhat@navgurukul.org>
2025-12-24 13:59:47 +0530

    api end point updated

--- features/assets/AssetsList.js
+++ features/assets/AssetsList.js
@@ -32,7 +32,7 @@ export default function AssetsList() {
   
   // Fetch assets data from API
   const { data, isLoading, isError, error } = useFetch({
-    url: 'http://13.203.90.62/assets',
+    url: 'http://13.203.90.62/api/assets',
     queryKey: ['assets'],
   });
 
@@ -112,7 +112,7 @@ export default function AssetsList() {
     setIsSubmitting(true);
     try {
       // Make API call to create asset
-      const response = await fetch('http://13.203.90.62/assets', {
+      const response = await fetch('http://13.203.90.62/api/assets', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
